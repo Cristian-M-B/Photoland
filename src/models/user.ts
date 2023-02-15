026602ab39ea0589d793bbd8fe7284418dbbc9f9
Cristian-M-B <crisbaronetto@hotmail.com>
2023-02-15 19:15:46 -0300

    add user model

---
+++ src/models/user.ts
@@ -0,0 +1,48 @@
+import { Schema, model, models } from 'mongoose'
+
+const userSchema = new Schema({
+        fullName: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        userName: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        password: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        private: {
+            type: Boolean,
+            default: false
+        },
+        picture: {
+            type: String
+        },
+        files: {
+            type: Array
+        },
+        albums: {
+            type: Array
+        }
+    },
+    {
+        timestamps: false,
+        versionKey: false
+    }
+)
+
+const User = models.User || model('User', userSchema)
+
+export default User
