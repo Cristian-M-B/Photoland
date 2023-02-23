f484567243f0002538b2ab7b60fce7878d20e444
Cristian-M-B <crisbaronetto@hotmail.com>
2023-02-23 18:05:57 -0300

    add postUser route

---
+++ src/pages/api/postUser.ts
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import connectionDB from '../../utils/mongodb'
+import User from '../../models/user'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    await connectionDB();
+    const newUser = new User({
+      fullName: req.body.fullName,
+      userName: req.body.userName,
+      email: req.body.email,
+      password: req.body.password
+    })
+    const userSaved = await newUser.save();
+    res.status(200).json(userSaved)
+  } catch (error) {
+    console.log(error)
+  }
+}
