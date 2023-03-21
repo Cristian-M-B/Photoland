e2c27055b788fd8d9a51d9646827bee925d58f4a
Cristian-M-B <crisbaronetto@hotmail.com>
2023-03-21 18:32:41 -0300

    change user interface

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,3 +1,5 @@
+import { IFile } from './publication'
+
 export interface IUserData {
     fullName: string,
     userName: string,
@@ -8,19 +10,5 @@ export interface IUserData {
 export default interface IUser extends IUserData {
     _id: string,
     private: boolean,
-    picture?: string,
-    files: IFiles[],
-    albums: IAlbums[]
-}
-
-export interface IFiles {
-    id: string,
-    url: string,
-    type: string,
-    date: string
-}
-
-interface IAlbums {
-    name: string,
-    files: IFiles[]
+    picture?: IFile
 }
