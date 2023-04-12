aab5e7e661322204b57819c0c3e12a23ae0a3775
Cristian-M-B <crisbaronetto@hotmail.com>
2023-04-12 17:39:22 -0300

    add token and cookie

--- src/pages/api/postUser.ts
+++ src/pages/api/postUser.ts
@@ -1,10 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import connectionDB from '../../utils/mongodb'
 import User from '../../models/user'
+import jwt from 'jsonwebtoken'
+import { setCookie } from 'cookies-next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    await connectionDB()
+    connectionDB()
     const newUser = new User({
       fullName: req.body.fullName,
       userName: req.body.userName,
@@ -12,6 +14,19 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       password: req.body.password
     })
     const userSaved = await newUser.save()
+    const token = jwt.sign({
+      expiresIn: '30d',
+      userID: `${userSaved._id}`
+    }, process.env.JWT_SECRET || '')
+    setCookie('Photoland', token, {
+      req,
+      res,
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 30 * 24 * 60 * 60,
+      path: '/'
+    })
     res.status(200).json(userSaved)
   } catch (error) {
     console.log(error)
