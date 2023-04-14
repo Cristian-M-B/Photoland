717773f91ef6ae8fab8986579edea55707f891f2
Cristian-M-B <crisbaronetto@hotmail.com>
2023-04-14 14:23:02 -0300

    add getUser route

---
+++ src/pages/api/getUser.ts
@@ -0,0 +1,32 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import connectionDB from '../../utils/mongodb'
+import User from '../../models/user'
+import jwt from 'jsonwebtoken'
+import { setCookie } from 'cookies-next'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        connectionDB()
+        const user = await User.findOne({ email: req.body.email }).lean()
+        const password = user?.password === req.body.password ? true : false
+        if (password) {
+            const token = jwt.sign({
+                expiresIn: '30d',
+                userID: `${user._id}`
+            }, process.env.JWT_SECRET || '')
+            setCookie('Photoland', token, {
+                req,
+                res,
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production',
+                sameSite: 'strict',
+                maxAge: 30 * 24 * 60 * 60,
+                path: '/'
+            })
+            return res.status(200).json(user)
+        }
+    } catch (error) {
+        console.log(error)
+    }
+    res.status(200).send('El email o la contraseña son incorrectos')
+}
