299d165493d4ee3b6b4ce8e0213c4858f94dcfeb
Cristian-M-B <crisbaronetto@hotmail.com>
2023-04-28 11:30:19 -0300

    add NavBar component

--- src/pages/[userName]/index.tsx
+++ src/pages/[userName]/index.tsx
@@ -4,21 +4,29 @@ import User from '../../models/user'
 import Publication from '../../models/publication'
 import IUser from '../../types/user'
 import { IPublication } from '../../types/publication'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { getCookie } from 'cookies-next'
+import jwt from 'jsonwebtoken'
+import NavBar from '../../components/NavBar'
 import ProfileData from '../../components/ProfileData'
 import IconsMenu from '../../components/IconsMenu'
 import UserFiles from '../../components/UserFiles'
 
 interface Props {
     user: IUser,
-    allPublications: IPublication[]
+    allPublications: IPublication[],
+    allUsers: IUser[],
+    userSession: IUser
 }
 
-export default function Profile({ user, allPublications }: Props) {
+export default function Profile({ user, allPublications, allUsers, userSession }: Props) {
     const [userProfile, setUserProfile] = useState<IUser>(user)
     const [publications, setPublications] = useState<IPublication[]>(allPublications)
+    const [currentUser, setCurrentUser] = useState<IUser>(userSession)
 
     return (
         <>
+            <NavBar allUsers={allUsers} currentUser={currentUser} setCurrentUser={setCurrentUser} />
             <ProfileData
                 userProfile={userProfile}
                 setUserProfile={setUserProfile}
@@ -36,20 +44,44 @@ export default function Profile({ user, allPublications }: Props) {
 }
 
 interface Props {
-    params: IUser
+    params: IUser,
+    req: NextApiRequest,
+    res: NextApiResponse
 }
 
-export async function getServerSideProps({ params }: Props) {
+interface Token {
+    userID: string,
+    iat: number,
+    expiresIn: number
+}
+
+export async function getServerSideProps({ params, req, res }: Props) {
     connectionDB()
+    const allUsers = await User.find({}).lean()
     const user = await User.findOne({ userName: params.userName }).lean()
+
+    const token = getCookie('Photoland', { req, res })
+    let userID: string = ''
+    let userSession
+
+    try {
+        const session = jwt.verify((token as string), process.env.JWT_SECRET || '')
+        userID = (session as Token).userID
+        userSession = await User.findById(userID).lean()
+    } catch (error) {
+        // console.log(error)
+    }
+
 
     if (user) {
         let allPublications = await Publication.find({}).populate('user').lean()
         allPublications = allPublications?.filter(publication => publication.user.userName === params.userName)
         return {
             props: {
+                allUsers: JSON.parse(JSON.stringify(allUsers)),
                 user: JSON.parse(JSON.stringify(user)),
-                allPublications: JSON.parse(JSON.stringify(allPublications.reverse()))
+                allPublications: JSON.parse(JSON.stringify(allPublications.reverse())),
+                userSession: JSON.parse(JSON.stringify(userSession || null))
             }
         }
     }
