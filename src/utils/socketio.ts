8b9836f492a8dc49186efa299ff3c97b77d80e81
Cristian-M-B <crisbaronetto@hotmail.com>
2023-05-08 13:43:21 -0300

    add connect, newUser, newNotification, showNotification, deleteUser and disconnect functions

--- src/utils/socketio.ts
+++ src/utils/socketio.ts
@@ -1,7 +1,36 @@
-import { io } from 'socket.io-client'
+import { Dispatch, SetStateAction } from 'react'
+import { io, Socket } from 'socket.io-client'
+import IUser, { INotification } from '../types/user'
 
-export default function getSocketIO() {
+let socket: Socket
+const PHOTOLAND_URL = process.env.PHOTOLAND_URL || ''
+
+export default function connect() {
     fetch('/api/socket')
-    const socket = io('http://localhost:3000')
-    return socket
+    socket = io(`${PHOTOLAND_URL}`)
+}
+
+export function newUser(currentUser: IUser) {
+    socket.emit('newUser', currentUser)
+}
+
+export function newNotification(publicationUserID: string, notification: INotification) {
+    socket.emit('newNotification', publicationUserID, notification)
+}
+
+export function showNotification(notifications: INotification[], setNotifications: Dispatch<SetStateAction<INotification[]>>) {
+    socket.on('showNotification', notification => {
+        setNotifications([
+            ...notifications,
+            notification
+        ])
+    })
+}
+
+export function deleteUser(currentUser: IUser) {
+    socket.emit('deleteUser', currentUser)
+}
+
+export function disconnect() {
+    socket.disconnect()
 }
