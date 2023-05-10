d7ba2c8d61ffec7608c4cf57f0fe727b7f9fe8fe
Cristian-M-B <crisbaronetto@hotmail.com>
2023-05-10 13:57:40 -0300

    change user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,39 +1,46 @@
 import { Schema, model, models } from 'mongoose'
 
 const userSchema = new Schema({
-        fullName: {
-            type: String,
-            required: true,
-            trim: true
-        },
-        userName: {
-            type: String,
-            required: true,
-            unique: true,
-            trim: true
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            trim: true
-        },
-        password: {
-            type: String,
-            required: true,
-            trim: true
-        },
-        private: {
-            type: Boolean,
-            default: false
-        },
-        picture: {
-            type: Object
-        },
-        notifications: {
-            type: Array
-        }
+    fullName: {
+        type: String,
+        required: true,
+        trim: true
     },
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    private: {
+        type: Boolean,
+        default: false
+    },
+    picture: {
+        type: Object
+    },
+    notifications: [{
+        type: { type: String },
+        isRead: { type: Boolean },
+        isIgnored: { type: Boolean },
+        publicationID: { type: String },
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    }]
+},
     {
         timestamps: false,
         versionKey: false
