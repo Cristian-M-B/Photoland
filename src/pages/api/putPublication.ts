cbf5fac09a254d6f4060586de24c2bd286863292
Cristian-M-B <crisbaronetto@hotmail.com>
2023-05-12 10:14:07 -0300

    modification

--- src/pages/api/putPublication.ts
+++ src/pages/api/putPublication.ts
@@ -7,7 +7,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         connectionDB()
         await Publication.findByIdAndUpdate(
             req.body.publicationID,
-            { $addToSet: { likes: [req.body.userLike] } }
+            { $addToSet: { likes: req.body.userLike } }
         )
         const update = await Publication.findById(req.body.publicationID).populate('user').lean()
         res.status(200).json(update.likes)
