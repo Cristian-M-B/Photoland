80582490d4d8de83389cbac5034033617ad8f6ba
Cristian-M-B <crisbaronetto@hotmail.com>
2023-05-12 10:15:25 -0300

    modification

--- src/pages/[userName]/index.tsx
+++ src/pages/[userName]/index.tsx
@@ -67,7 +67,14 @@ export async function getServerSideProps({ params, req, res }: Props) {
     try {
         const session = jwt.verify((token as string), process.env.JWT_SECRET || '')
         userID = (session as Token).userID
-        userSession = await User.findById(userID).lean()
+        userSession = await User.findById(userID).populate({
+            path: 'notifications',
+            populate: {
+                path: 'user',
+                model: 'User',
+                select: 'userName picture'
+            }
+        }).lean()
     } catch (error) {
         // console.log(error)
     }
