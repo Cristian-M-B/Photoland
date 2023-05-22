9033dec217c149a9f548de648dc26977f6fd8c47
Cristian-M-B <crisbaronetto@hotmail.com>
2023-05-22 16:09:24 -0300

    update notifications

--- src/pages/api/putUser.ts
+++ src/pages/api/putUser.ts
@@ -3,11 +3,12 @@ import { v2 as cloudinary } from 'cloudinary'
 import connectWithCloudinary from '../../utils/cloudinary'
 import connectionDB from '../../utils/mongodb'
 import User from '../../models/user'
+import { INotification } from '../../types/user'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    connectionDB()
     if (req?.query?.notification === 'true') {
         try {
-            connectionDB()
             const newNotification = { ...req.body.notification, user: req.body.notification.user._id }
             const user = await User.findById(req.body.publicationUserID).lean()
             const notifications = [newNotification, ...user.notifications]
@@ -16,9 +17,24 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         } catch (error) {
             console.log(error)
         }
+    } else if (req?.query?.notificationRead === 'true') {
+        try {
+            const user = await User.findById(req.body.userID).lean()
+            const updateNotifications = user.notifications.map((notification: INotification) => {
+                if (notification?._id?.toString() === req.body.notificationID.toString()) {
+                    return { ...notification, isRead: true }
+                } else {
+                    return { ...notification }
+                }
+            })
+            const filteredNotifications = updateNotifications.filter((notification: INotification) => notification.isRead === false)
+            await User.findByIdAndUpdate(req.body.userID, { notifications: filteredNotifications })
+            res.status(200).json(filteredNotifications)
+        } catch (error) {
+            console.log(error)
+        }
     } else {
         try {
-            connectionDB()
             const user = await User.findOne({ userName: req.query.userName }).lean()
             if (user?.picture?.url) {
                 connectWithCloudinary()
