2af6e7f64c3a70d15e69fcee2adbb2584f3b8c08
Cristian-M-B <crisbaronetto@hotmail.com>
2023-06-15 12:49:05 -0300

    add getUserByID

--- src/pages/api/getUser.ts
+++ src/pages/api/getUser.ts
@@ -5,28 +5,37 @@ import jwt from 'jsonwebtoken'
 import { setCookie } from 'cookies-next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    try {
-        connectionDB()
-        const user = await User.findOne({ email: req.body.email }).lean()
-        const password = user?.password === req.body.password ? true : false
-        if (password) {
-            const token = jwt.sign({
-                expiresIn: '30d',
-                userID: `${user._id}`
-            }, process.env.JWT_SECRET || '')
-            setCookie('Photoland', token, {
-                req,
-                res,
-                httpOnly: true,
-                secure: process.env.NODE_ENV === 'production',
-                sameSite: 'strict',
-                maxAge: 30 * 24 * 60 * 60,
-                path: '/'
-            })
-            return res.status(200).json(user)
+    connectionDB()
+    if(req?.query?.id) {
+        try {
+            const user = await User.findById(req.query.id)
+            res.status(200).json(user)
+        } catch (error) {
+            console.log(error)
         }
-    } catch (error) {
-        console.log(error)
+    } else {
+        try {
+            const user = await User.findOne({ email: req.body.email }).lean()
+            const password = user?.password === req.body.password ? true : false
+            if (password) {
+                const token = jwt.sign({
+                    expiresIn: '30d',
+                    userID: `${user._id}`
+                }, process.env.JWT_SECRET || '')
+                setCookie('Photoland', token, {
+                    req,
+                    res,
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === 'production',
+                    sameSite: 'strict',
+                    maxAge: 30 * 24 * 60 * 60,
+                    path: '/'
+                })
+                return res.status(200).json(user)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+        res.status(200).send('El email o la contraseña son incorrectos')
     }
-    res.status(200).send('El email o la contraseña son incorrectos')
 }
