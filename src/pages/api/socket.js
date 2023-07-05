a11bb0b1be65c2eff0c3fedfa78294ddcd037124
Cristian-M-B <crisbaronetto@hotmail.com>
2023-07-05 12:11:43 -0300

    modify

--- src/pages/api/socket.js
+++ src/pages/api/socket.js
@@ -20,7 +20,7 @@ export default function handler(req, res) {
     if (!res.socket.server.io) {
         console.log('Starting socket.io')
 
-        const io = new Server(res.socket.server, {path:'/api/socket'})
+        const io = new Server(res.socket.server, {path:'/api/socket', cors: {origin: '*'}})
 
         io.on('connection', (socket) => {
             console.log(`Connected: ${socket.id}`)
